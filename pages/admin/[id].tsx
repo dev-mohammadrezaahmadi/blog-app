9b6375c5a1de474169d8706efeacd8a3fba18dd0
dev-mohammadrezaahmadi <dev.mohammadrezaahmadi@gmail.com>
2021-08-03 20:21:28 +0430

    add administration page for future autherization

---
+++ pages/admin/[id].tsx
@@ -0,0 +1,5 @@
+const AdminPostEdit = () => {
+	return <div></div>;
+};
+
+export default AdminPostEdit;
---
+++ pages/admin/index.tsx
@@ -0,0 +1,5 @@
+const AdminPostsPage = () => {
+	return <div></div>;
+};
+
+export default AdminPostsPage;
