25464453c6ec472654acbcc5bdd5462655f21441
dev-mohammadrezaahmadi <dev.mohammadrezaahmadi@gmail.com>
2021-08-03 20:23:14 +0430

    make navbar component and add it to _app file to be consistent in all pages

---
+++ components/Navbar.tsx
@@ -0,0 +1,53 @@
+import Link from "next/link";
+
+const Navbar = () => {
+	const user = true;
+	const username = true;
+
+	return (
+		<nav className="h-20 w-full bg-white text-black fixed top-0 font-bold z-50 shadow-md">
+			<ul className="list-none m-0 p-0 flex items-center justify-evenly h-full">
+				<li className="rounded-full">
+					<Link href="/">
+						<button className="btn bg-black text-white text-2xl">FEED</button>
+					</Link>
+				</li>
+
+				{/* user is signed-in and has username */}
+				{username && (
+					<>
+						<li>
+							<Link href="/admin">
+								<button className="btn bg-blue-700 text-white text-xl">
+									Write Posts
+								</button>
+							</Link>
+						</li>
+						<li>
+							<Link href={`/${username}`}>
+								<img
+									className="cursor-pointer border-black h-8 w-8 rounded-full"
+									src={user?.photoURL}
+									alt=""
+								/>
+							</Link>
+						</li>
+					</>
+				)}
+
+				{/* user is not signed or has not created username */}
+				{!username && (
+					<li>
+						<Link href="/enter">
+							<button className="btn text-xl bg-blue-700 text-white">
+								Log in
+							</button>
+						</Link>
+					</li>
+				)}
+			</ul>
+		</nav>
+	);
+};
+
+export default Navbar;
